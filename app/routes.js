e538dc731e6f8996980d64d669f64c8f6cc92db2
dwisetiyadi <dwi.setiyadi@gmail.com>
2014-03-29 00:43:20 +0700

    add example of controller and middleware

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,6 @@ module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('hello');
     });
+
+    app.get('/foo', common.middleware('foo').bar, common.conroller('foo').bar);
 }
