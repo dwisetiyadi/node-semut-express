6cdb11f6c5a4c5128d816100f7025a40e85e95f3
dwisetiyadi <dwi.setiyadi@gmail.com>
2014-03-29 21:22:40 +0700

    remove load config on common.js, and minor change on hello.jade

--- app/libraries/common.js
+++ app/libraries/common.js
@@ -1,23 +1,21 @@
 // Call dependencies
 var path     = require('path');
-var rootPath = path.normalize(__dirname + '/../..')
-var env      = process.env.NODE_ENV || 'development';
-var config   = require(rootPath + '/app/config/app')[env];
+var appPath = path.normalize(__dirname + '/..');
 
 /**
  * Expose common
  */
 module.exports = {
     model : function (callFile) {
-        var modelPath = config.path.app + '/models/' + callFile + '.js';
+        var modelPath = appPath + '/models/' + callFile + '.js';
         return require(modelPath);
     },
     middleware: function (callFile) {
-        var middlewarePath = config.path.app + '/middlewares/' + callFile + '.js';
+        var middlewarePath = appPath + '/middlewares/' + callFile + '.js';
         return require(middlewarePath);
     },
     controller: function (callFile) {
-        var controllerPath = config.path.app + '/controllers/' + callFile + '.js';
+        var controllerPath = appPath + '/controllers/' + callFile + '.js';
         return require(controllerPath);
     }
 };
